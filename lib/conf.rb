97ae0ac1bb1c7f0c7dafd496ecfb910c7a59be85
ina <ina@kuis.kyoto-u.ac.jp>
2011-07-21 17:04:22 +0900

    local can be nil.

--- lib/conf.rb
+++ lib/conf.rb
@@ -1,7 +1,7 @@
 class App
   class Conf
     def initialize(master, local)
-      @hash = master.merge(local)
+      @hash = master.merge(local||{})
     end
 
     def [](*keys) return keys.inject(@hash){|r,x| r[x.to_s]} end
