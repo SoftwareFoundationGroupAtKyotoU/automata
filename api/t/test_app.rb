e93be366f3974660fef21e101ac5b6c4e3366010
chiro <peanut2626@gmail.com>
2014-12-24 16:57:58 +0900

    Add user_names_from_tokens method to App class.
    This function will be used by comment.cgi to show user names
    instead of login-ids in a comment tab.

---
+++ api/t/test_app.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+
+require 'app'
+
+class AppText < Test::Unit::TestCase
+  def setup
+    @app = App.new
+    class <<@app
+      def users(all = false)
+        [User.new(
+          { 'number' => 1234567890,
+            'login'  => 'login-id1',
+            'name'   => 'user name1',
+            'ruby'   => 'user ruby1',
+            'email'  => '1a@b.c'
+          }),
+         User.new(
+           { 'number' => 1234567892,
+             'login'  => 'login-id2',
+             'name'   => 'user name2',
+             'ruby'   => 'user ruby2',
+             'email'  => '2a@b.c'
+           })]
+      end
+    end
+
+    @user1 = @app.users[0]
+    @user2 = @app.users[1]
+  end
+
+  def test_user_names_from_token
+    @user_token1 = User.make_token(@user1.real_login)
+    @user_token2 = User.make_token(@user2.real_login)
+    assert_equal({@user_token1 => @user1.name}, @app.user_names_from_tokens([@user_token1]))
+    assert_equal({@user_token1 => @user1.name, @user_token2 => @user2.name},
+                 @app.user_names_from_tokens([@user_token1, @user_token2]))
+  end
+end
