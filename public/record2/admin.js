101262163fff3f27a9d73faa91c713c95f4eb641
M.Kobayashi <nov22.mk@gmail.com>
2015-03-11 13:27:33 +0900

    now rewriting log_view...

---
+++ public/record2/admin.js
@@ -0,0 +1,23 @@
+var Admin = function(update) {
+    if (typeof update != 'function') update = update.record;
+    var command = function(cmd, args, callback, error) {
+        apiPost('admin_'+cmd, args, callback, error);
+    };
+
+    return {
+        editStatus: function(args, callback, error) {
+            command('log', args, callback, error);
+        },
+        editLog: function(args, callback, error) {
+            command('log', args, callback, error);
+        },
+        editSolved: function(args, callback, error) {
+            command('solved', args, callback, error);
+        },
+        runTest: function(user, report, callback, error) {
+            var args = { user: user, report: report };
+            command('runtest', args, callback, error);
+        },
+        update: update
+    };
+};
