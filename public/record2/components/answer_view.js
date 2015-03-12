10e3120d3f0ddfb11edabb7931aadaaff19ca301
Takeshi Omoto <omoto@fos.kuis.kyoto-u.ac.jp>
2015-03-12 20:30:11 +0900

    Rewrite answer_view.js with React

--- public/record2/components/answer_view.js
+++ public/record2/components/answer_view.js
@@ -1,27 +1,38 @@
-var Hoge = React.createClass({
-    getInitialState: function() {
-        return {
-            count: 0
-        };
-    },
-
-    // componentWillMount: function() {
-    //     console.log('will mount ' + this.props.count);
-    // },
-
-    // componentWillReceiveProps: function() {
-    //     console.log('will receive ' + this.props.count);
-    //     this.setState({
-    //         count: this.props.count
-    //     });
-    // },
-    componentDidMount: function() {
-        console.log('did mount ' + this.props.count);
-    },
-
+var Solved = React.createClass({
     render: function() {
+        var solved = this.props.solved.map(function(s) {
+            return (
+                <li>{s}</li>
+            )});
         return (
-                <p>hoge: {this.props.count}</p>
+                <ul>
+                <h3>解答済み</h3>
+                {solved}
+                </ul>
+        );
+    }
+});
+
+var Unsolved = React.createClass({
+    render: function () {
+        var unsolved;
+
+        if (this.props.unsolved.length != []) {
+            unsolved = this.props.unsolved.map(function(us) {
+                return (
+                        <li>{us[0]}</li>
+                )
+            });
+        }
+        else {
+            unsolved = (<li>なし</li>);
+            };
+
+        return (
+                <ul>
+                <h3>未回答</h3>
+                {unsolved}
+                </ul>
         );
     }
 });
@@ -29,23 +40,103 @@ var Hoge = React.createClass({
 var AnswerView = React.createClass({
     getInitialState: function() {
         return {
-            a: 0
+            solved_list: {solved:[]},
+            unsolved_list: {unsolved:[]},
+            clicked: false,
+            mounted: false
         };
     },
 
     componentDidMount: function() {
-        setInterval(function() { this.setState({ a: this.state.a + 1 }) }.bind(this), 1000);
+        $.get('../api/user.cgi',
+              {
+                  user: this.props.token,
+                  report: this.props.report,
+                  type: 'status',
+                  status: 'solved',
+                  action: 'get',
+              },
+              function(result) {
+                  this.setState({
+                      solved_list: result[0].report[this.props.report]
+                  });
+              }.bind(this));
+        $.get('../api/user.cgi',
+              {
+                  user: this.props.token,
+                  report: this.props.report,
+                  type: 'status',
+                  status: 'record',
+                  action: 'get',
+              },
+              function(result) {
+                  this.setState({
+                      unsolved_list: result[0].report[this.props.report],
+                      mounted: true
+                  });
+              }.bind(this));
+    },
+
+    onClick: function() {
+        this.setState({
+            clicked: !this.state.clicked
+        });
+    },
+
+    toolBar: function() {
+        return (
+                <ul className="status_toolbar">
+                <li className="toolbutton">
+                <a onClick={this.onClick}>✏編集</a>
+                </li>
+                </ul>
+        );
+    },
+
+    posted: function(solved, unsolved) {
+        this.setState({
+            clicked: false,
+            solved_list: solved,
+            unsolved_list: unsolved,
+        });
     },
 
     render: function() {
-        return (
-                <div className="status_window">
-                <StatusHeader tabName='answer'/>
-                <div className="status_view">AnswerView: 構築中</div>
-                <p>{this.state.a}</p>
-                <Hoge count={this.state.a}/>
-                <p><p><div id="hogehoge">hogehoge</div></p></p>
-                </div>
-        );
+        if (!this.state.mounted) {
+            return (
+                    <div className="status_window">
+                    <StatusHeader tabName='answer' toolBar={this.toolBar}/>
+                    <div className="status_view">
+                    <img src="./loading.gif" />
+                    </div>
+                    </div>
+            );
+        }
+        else if(!this.state.clicked && this.state.mounted) {
+            return (
+                    <div className="status_window">
+                    <StatusHeader tabName='answer' toolBar={this.toolBar}/>
+                    <div className="status_view">
+                    <Solved solved={this.state.solved_list.solved}/>
+                    <Unsolved unsolved={this.state.unsolved_list.unsolved}/>
+                    </div>
+                    </div>
+            );
+        }
+        else {
+            return (
+                    <div className="status_window">
+                    <StatusHeader tabName='answer'/>
+                    <div className="status_view">
+                    <AnswerEdit token={this.props.token}
+                                report={this.props.report}
+                                solved={this.state.solved_list.solved}
+                                unsolved={this.state.unsolved_list.unsolved}
+                                onclick={this.onClick}
+                                posted={this.posted} />
+                    </div>
+                    </div>
+            );
+        };
     }
 });
