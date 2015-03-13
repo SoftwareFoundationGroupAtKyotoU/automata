4f541601b21b5e02ca09a3ff478eb8f41d568710
yf-fyf <yousuke1204@gmail.com>
2015-03-14 00:13:53 +0900

    Reflect Hanada's review

--- js/record2/comment_view.js
+++ js/record2/comment_view.js
@@ -9,14 +9,13 @@ var Link = Router.Link;
 var Route = Router.Route;
 var RouteHandler = Router.RouteHandler;
 
-const ACL_MESSAGE_FOR_ALL      = "全員に公開";
-const ACL_MESSAGE_FOR_USER     = "提出者に公開";
-const ACL_MESSAGE_FOR_OTHER    = "提出者以外に公開";
-const ACL_MESSAGE_FOR_PRIVATE  = "非公開";
-const mode = { normal: 0, edit: 1, preview: 2 };
+var ACL_MESSAGE_FOR_ALL      = "全員に公開";
+var ACL_MESSAGE_FOR_USER     = "提出者に公開";
+var ACL_MESSAGE_FOR_OTHER    = "提出者以外に公開";
+var ACL_MESSAGE_FOR_PRIVATE  = "非公開";
+var mode = { normal: 0, edit: 1, preview: 2 };
 
-function formAclArgument(userFlag, otherFlag)
-{
+function formAclArgument(userFlag, otherFlag) {
     var aclAry = [];
     if (userFlag) { aclAry.push('user'); }
     if (otherFlag) { aclAry.push('other'); }
@@ -29,16 +28,21 @@ var Comment = React.createClass({
             modeState: mode['normal'],
             commentHTML: this.props.comment.content,
             commentText: '',
-            aclUserFlag: this.props.acl.indexOf('user') != -1,
-            aclOtherFlag: this.props.acl.indexOf('other') != -1,
+            aclUserFlag: this.props.acl.indexOf('user') !== -1,
+            aclOtherFlag: this.props.acl.indexOf('other') !== -1,
             loadingFlag: false
         };
     },
-    setMode: function(mode_text)  { this.setState({modeState: mode[mode_text]}); },
+    setMode: function(mode_text) {
+        this.setState({modeState: mode[mode_text]});
+    },
     normalMode: function()        { this.setMode('normal'); },
     editMode: function()          { this.setMode('edit'); },
     previewMode: function()       { this.setMode('preview'); },
-    isMode: function(mode_text)   { return this.state.modeState == mode[mode_text]; },
+
+    isMode: function(mode_text) {
+        return this.state.modeState == mode[mode_text];
+    },
     isNormalMode: function()      { return this.isMode('normal'); },
     isEditMode: function()        { return this.isMode('edit'); },
     isPreviewMode: function()     { return this.isMode('preview'); },
@@ -63,8 +67,10 @@ var Comment = React.createClass({
                 },
                 function(result) {
                     this.setCommentHTML(result[0].content);
-                    this.setState({ aclUserFlag: result[0].acl.indexOf('user') != -1,
-                                    aclOtherFlag: result[0].acl.indexOf('other') != -1 });
+                    this.setState({
+                        aclUserFlag: result[0].acl.indexOf('user') !== -1,
+                        aclOtherFlag: result[0].acl.indexOf('other') !== -1
+                    });
                     this.loadingModeEnd();
                     cont();
                 }.bind(this));
@@ -85,11 +91,14 @@ var Comment = React.createClass({
                 report: this.props.report,
                 action: 'edit',
                 id: this.props.comment.id,
-                acl: formAclArgument(this.state.aclUserFlag, this.state.aclOtherFlag),
+                acl: formAclArgument(this.state.aclUserFlag,
+                                     this.state.aclOtherFlag),
                 message: this.getCommentText()
             },
             complete: function(data) {
-                this.reloadComment(function() { this.normalMode(); }.bind(this));
+                this.reloadComment(function() {
+                    this.normalMode();
+                }.bind(this));
             }.bind(this)});
     },
 
@@ -164,24 +173,43 @@ var Comment = React.createClass({
 
     render: function() {
         if (this.state.loadingFlag) {
-            return <li><div className="form"><img src="../record/loading.gif" /></div></li>;
+            return (
+                <li>
+                    <div className="form">
+                        <img src="../record/loading.gif" />
+                    </div>
+                </li>
+            );
         }
 
         const buttons = (
             <span>
-                <input type="submit" value="コメントする" onClick={this.onComment}></input>
-                <input type="button" value="プレビュー" onClick={this.onPreview}></input>
-                <input type="button" value="キャンセル" onClick={this.onCancel}></input>
+                <input type="submit" value="コメントする"
+                       onClick={this.onComment}></input>
+                <input type="button" value="プレビュー"
+                       onClick={this.onPreview}></input>
+                <input type="button" value="キャンセル"
+                       onClick={this.onCancel}></input>
             </span>
         );
         var checkBox;
         if (this.props.admin) {
             checkBox = (
                 <span>
-                    <input id="summary-report2_comment_acl_user" type="checkbox" name="user" checked={this.state.aclUserFlag} onChange={this.onCheckAclUser} />
-                    <label htmlFor="summary-report2_comment_acl_user">{ACL_MESSAGE_FOR_USER}</label>
-                    <input id="summary-report2_comment_acl_other" type="checkbox" name="other" checked={this.state.aclOtherFlag} onChange={this.onCheckAclOther} />
-                    <label htmlFor="summary-report2_comment_acl_other">{ACL_MESSAGE_FOR_OTHER}</label>
+                    <input id="summary-report2_comment_acl_user"
+                           type="checkbox" name="user"
+                           checked={this.state.aclUserFlag}
+                           onChange={this.onCheckAclUser} />
+                    <label htmlFor="summary-report2_comment_acl_user">
+                        {ACL_MESSAGE_FOR_USER}
+                    </label>
+                    <input id="summary-report2_comment_acl_other"
+                           type="checkbox" name="other"
+                           checked={this.state.aclOtherFlag}
+                           onChange={this.onCheckAclOther} />
+                    <label htmlFor="summary-report2_comment_acl_other">
+                        {ACL_MESSAGE_FOR_OTHER}
+                    </label>
                 </span>
           );
         }
@@ -190,13 +218,17 @@ var Comment = React.createClass({
         if (this.isNormalMode()) {
             comment_box = (
                 <div className="form">
-                    <div className="message" dangerouslySetInnerHTML={{__html: this.getCommentHTML()}} />
+                    <div className="message"
+                         dangerouslySetInnerHTML={
+                            {__html: this.getCommentHTML()}
+                         } />
                 </div>
             );
         } else if (this.isEditMode()) {
             comment_box = (
                 <div className="form">
-                    <textarea rows="6" value={this.getCommentText()} onChange={this.handleChange} />
+                    <textarea rows="6" value={this.getCommentText()}
+                              onChange={this.handleChange} />
                     {buttons}
                     {checkBox}
                 </div>
@@ -204,7 +236,10 @@ var Comment = React.createClass({
         } else if (this.isPreviewMode()) {
             comment_box = (
                 <div className="form">
-                    <div className="preview message" dangerouslySetInnerHTML={{__html: this.getCommentHTML()}} />
+                    <div className="preview message"
+                        dangerouslySetInnerHTML={
+                            {__html: this.getCommentHTML()}
+                        } />
                     {buttons}
                     {checkBox}
                 </div>
@@ -229,14 +264,27 @@ var Comment = React.createClass({
             <div className="meta">
                 <p className="author">{this.props.comment.user_name}</p>
                 {editButtons}
-                <p className="acl">{this.getAclMessage(this.state.aclUserFlag, this.state.aclOtherFlag)}</p>
+                <p className="acl">{
+                    this.getAclMessage(this.state.aclUserFlag,
+                    this.state.aclOtherFlag)
+                }</p>
                 <p className="date">{this.props.comment.timestamp}</p>
             </div>
         );
         if (!this.state.aclUserFlag && !this.state.aclOtherFlag) {
-            return <li id={li_id} className="private">{div_meta}{comment_box}</li>;
+            return (
+                <li id={li_id} className="private">
+                    {div_meta}
+                    {comment_box}
+                </li>
+            );
         } else {
-            return <li id={li_id}>{div_meta}{comment_box}</li>;
+            return (
+                <li id={li_id}>
+                    {div_meta}
+                    {comment_box}
+                </li>
+            );
         }
     }
 });
@@ -254,13 +302,22 @@ var CommentForm = React.createClass({
     getIsPreview: function()    { return this.state.isPreview; },
     setTextValue: function(txt) { this.setState({ textValue: txt }); },
     getTextValue: function()    { return this.state.textValue; },
-    resetValue: function()      { this.setTextValue(''); this.setIsPreview(false); },
+    resetValue: function()      { this.setTextValue('');
+                                  this.setIsPreview(false); },
 
-    onCheckAclUser: function(event)   { this.setState({ aclUserFlag: !this.state.aclUserFlag }); },
-    onCheckAclOther: function(event)  { this.setState({ aclOtherFlag: !this.state.aclOtherFlag }); },
+    onCheckAclUser: function(event) {
+        this.setState({aclUserFlag: !this.state.aclUserFlag });
+    },
+    onCheckAclOther: function(event) {
+        this.setState({ aclOtherFlag: !this.state.aclOtherFlag });
+    },
 
-    onPreview: function(event)        { this.setIsPreview(!this.getIsPreview()); },
-    handleChange: function(event)     { this.setTextValue(event.target.value); },
+    onPreview: function(event) {
+        this.setIsPreview(!this.getIsPreview());
+    },
+    handleChange: function(event) {
+        this.setTextValue(event.target.value);
+    },
 
     onComment: function(event) {
         $.ajax({
@@ -270,7 +327,8 @@ var CommentForm = React.createClass({
                 user: [this.props.token],
                 report: this.props.report,
                 action: 'post',
-                acl:  formAclArgument(this.state.aclUserFlag, this.state.aclOtherFlag),
+                acl:  formAclArgument(this.state.aclUserFlag,
+                                      this.state.aclOtherFlag),
                 message: this.getTextValue()
             },
             complete: function(data) {
@@ -284,7 +342,8 @@ var CommentForm = React.createClass({
         var comment_area;
         if (!this.getIsPreview()) {
             comment_area = (
-                <textarea rows="6" value={this.getTextValue()} onChange={this.handleChange} />
+                <textarea rows="6" value={this.getTextValue()}
+                          onChange={this.handleChange} />
             );
         } else {
             comment_area = (
@@ -297,18 +356,30 @@ var CommentForm = React.createClass({
         if (this.props.admin) {
             checkBox = (
                 <span>
-                    <input id="summary-report2_comment_acl_user" type="checkbox" name="user" checked={this.state.aclUserFlag} onClick={this.onCheckAclUser} />
-                    <label htmlFor="summary-report2_comment_acl_user">{ACL_MESSAGE_FOR_USER}</label>
-                    <input id="summary-report2_comment_acl_other" type="checkbox" name="other" checked={this.state.aclOtherFlag} onClick={this.onCheckAclOther} />
-                    <label htmlFor="summary-report2_comment_acl_other">{ACL_MESSAGE_FOR_OTHER}</label>
+                    <input id="summary-report2_comment_acl_user"
+                           type="checkbox" name="user"
+                           checked={this.state.aclUserFlag}
+                           onClick={this.onCheckAclUser} />
+                    <label htmlFor="summary-report2_comment_acl_user">
+                        {ACL_MESSAGE_FOR_USER}
+                    </label>
+                    <input id="summary-report2_comment_acl_other"
+                           type="checkbox" name="other"
+                           checked={this.state.aclOtherFlag}
+                           onClick={this.onCheckAclOther} />
+                    <label htmlFor="summary-report2_comment_acl_other">
+                        {ACL_MESSAGE_FOR_OTHER}
+                    </label>
                 </span>
             );
         }
         return (
             <div className="form">
                 {comment_area}
-                <input type="submit" onClick={this.onComment} value="コメントする"/>
-                <input type="button" onClick={this.onPreview} value={preview_reedit_text} />
+                <input type="submit" onClick={this.onComment}
+                       value="コメントする"/>
+                <input type="button" onClick={this.onPreview}
+                       value={preview_reedit_text} />
                 {checkBox}
             </div>
         );
@@ -341,7 +412,9 @@ var CommentView = React.createClass({
     render: function() {
         var comments = this.state.comments.map(function(comment) {
             return (
-                <Comment comment={comment} admin={this.props.admin} token={this.props.token} report={this.props.report} acl={comment.acl} rerender={this.rerender}/>
+                <Comment comment={comment} admin={this.props.admin}
+                         token={this.props.token} report={this.props.report}
+                         acl={comment.acl} rerender={this.rerender}/>
             );
         }.bind(this));
         return (
@@ -350,7 +423,10 @@ var CommentView = React.createClass({
                     <ul className="comments">
                         {comments}
                         <li>
-                            <CommentForm admin={this.props.admin} token={this.props.token} report={this.props.report} rerender={this.rerender} />
+                            <CommentForm admin={this.props.admin}
+                                         token={this.props.token}
+                                         report={this.props.report}
+                                         rerender={this.rerender} />
                         </li>
                     </ul>
                 </div>
