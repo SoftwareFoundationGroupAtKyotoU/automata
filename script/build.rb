28b490050e23bc02b0e32ab53d7dd80bf4f62af2
Yuki Nishida <rsworks.snow@gmail.com>
2015-03-19 21:44:03 +0900

    Remove cleanup phase from build.rb

--- script/build.rb
+++ script/build.rb
@@ -35,10 +35,6 @@ files.each{|name, file| yml[name] = YAML.load_file(file)||{} rescue {} }
 
 app = App.new
 conf = {}
-[:default, report_id].each do |l|
-  ignore = app.conf[:master, :check, l, :ignore]
-  conf[:ignore] = ignore if ignore
-end
 [:build, :test].each do |k|
   conf[k] = {}
   [:default, report_id].each do |l|
@@ -77,13 +73,6 @@ src_files = Dir.glob("#{dir[:src]}/*", File::FNM_DOTMATCH)
 src_files.reject!{|f| f =~ /\/\.+$/}
 
 FileUtils.cp_r(src_files, dir[:target].to_s)
-
-# clean
-ignore = conf[:ignore] || []
-ignore = '(?:'+ignore.join('|')+')' if ignore.is_a?(Array)
-Dir.each_leaf(dir[:target].to_s, File::FNM_DOTMATCH) do |f|
-  FileUtils.rm(f) if f =~ /#{ignore}/
-end
 
 # make input file
 if !conf[:test].empty?
