0d84a75637885fd021aff65e8dbdb9f357e529f9
chiro <peanut2626@gmail.com>
2015-03-26 22:03:56 +0900

    Use jQuery instead of GNN.UI.

--- js/account/msg.js
+++ js/account/msg.js
@@ -1,14 +1,14 @@
-var UI = require('../gnn/ui');
+var $ = require('jquery');
 
 var msgs = {
-    done: "パスワードをメールで送信しました．",
-    notfound: "入力されたメールアドレスは登録されていませんでした．"
+    done: 'パスワードをメールで送信しました．',
+    notfound: '入力されたメールアドレスは登録されていませんでした．'
 };
 
 function init() {
     var status = location.hash.substring(1);
-    if(msgs[status]) {
-        UI.$('msg').appendChild(UI.$text(msgs[status]));
+    if(typeof msgs[status] !== 'undefined') {
+        $('#msg').text(msgs[status]);
     }
 }
 
