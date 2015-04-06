9218204867bb2898bfd24905c86b61d19d0d76f8
Hiroaki Inoue <hinoue@kuis.kyoto-u.ac.jp>
2015-04-06 22:11:59 +0900

    Separate applet code and add comments

--- lib/api/browse.rb
+++ lib/api/browse.rb
@@ -34,6 +34,7 @@ module API
   #     - pathがバイナリファイルを指すとき:
   #       { type: 'bin' }
   class Browse
+
     def call(env)
       helper = Helper.new(env)
       app = App.new(env['REMOTE_USER'])
@@ -64,25 +65,6 @@ module API
       path = path.respond_to?(:realpath) ? path.realpath : nil
       return helper.forbidden unless src && path
       return helper.forbidden unless path.to_s.index(src.to_s) == 0
-
-      class_dir = path.parent
-      realpath = class_dir.relative_path_from(src)
-      applet_code = "code=\"#{File.basename(path.to_s, '.*')}\""
-      applet_codebase =
-        'codebase="../browse/' +
-        [::User.make_token(user).to_s, report_id, realpath.to_s].join('/') + '"'
-      jar_path = src.relative_path_from(class_dir) + Pathname('../../../jar/')
-      libs = app.conf[:master, :check, :default, :applet, :java_library]
-      w = app.conf[:master, :check, :default, :applet, :width] || 500
-      h = app.conf[:master, :check, :default, :applet, :height] || 400
-      if libs.nil? || libs.empty?
-        applet_archive = ''
-      else
-        applet_archive =
-          "archive=\"#{ (libs.map { |item| jar_path + item }).join(',') }\""
-      end
-      applet_width = "width=\"#{w}\""
-      applet_height = "height=\"#{h}\""
 
       if path.directory?
         files = path.children.sort do |a, b|
@@ -120,8 +102,59 @@ module API
         return helper.json_response({'type' => 'txt', 'body' => result})
       elsif '.class' == path.extname && 'highlight' == helper.params['type']
         # return html including applet tag when .class file is selected
+        applet_html = self.gen_applet_html(src, path, user, app, report_id)
+        helper.json_response({'type' => 'txt', 'body' => applet_html})
+      elsif  helper.params['type'] == 'highlight'
+        helper.json_response({'type' => 'bin'})
+      else
+        header = {
+          'Content-Type' => MIME.check(path.to_s).to_s,
+          'Content-Length' => path.size
+        }
+        content = nil
+        File.open(path.to_s, 'rb') { |f| content = f.read }
+        Rack::Response.new([content], 200, header)
+      end
+    end
 
-        applet_html = <<"APPLET"
+
+    def gen_applet_html(src, path, user, app, report_id)
+      # applet tag consists of five attributes, 'code', 'codebase', 'archive', 'height' and 'width'
+      # 'code' is a name of main class
+      # 'codebase' is relative_path from "/public/record/" to a path where .class files exist
+      # 'archive' is relative path from 'codebase' to "/public/jar/"
+      
+      # name of main class
+      applet_code = "code=\"#{File.basename(path.to_s, '.*')}\""
+
+      # base_path means dir where .class files exist
+      base_path = path.parent
+      base_path_from_src = base_path.relative_path_from(src)      
+
+      # relative path from "/public/record/" to base_path
+      applet_codebase =
+        'codebase="../browse/' +
+        [::User.make_token(user).to_s, report_id, base_path_from_src.to_s].join('/') + '"'
+
+      # archive_path_from_src means 
+      # Pathname('/public/jar/').relative_path_from(Pathname('/public/browse/user_id/kadai_id/'))
+      archive_path_from_src = Pathname('../../../jar/')
+      libs = app.conf[:master, :check, :default, :applet, :java_library]
+      if libs.nil? || libs.empty?
+        applet_archive = ''
+      else
+        archive_path_from_class = src.relative_path_from(base_path) + archive_path_from_src
+        applet_archive =
+          "archive=\"#{ (libs.map { |item| archive_path_from_class + item }).join(',') }\""
+      end
+
+      # width and height of applet view
+      w = app.conf[:master, :check, :default, :applet, :width] || 500
+      h = app.conf[:master, :check, :default, :applet, :height] || 400
+      applet_width = "width=\"#{w}\""
+      applet_height = "height=\"#{h}\""
+      
+      applet_html = <<"APPLET"
       <?xml version="1.0" encoding="utf-8"?>
       <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html xmlns="http://www.w3.org/1999/xhtml">
@@ -139,19 +172,8 @@ module API
         </body>
       </html>
 APPLET
-
-        helper.json_response({'type' => 'txt', 'body' => applet_html})
-      elsif  helper.params['type'] == 'highlight'
-        helper.json_response({'type' => 'bin'})
-      else
-        header = {
-          'Content-Type' => MIME.check(path.to_s).to_s,
-          'Content-Length' => path.size
-        }
-        content = nil
-        File.open(path.to_s, 'rb') { |f| content = f.read }
-        Rack::Response.new([content], 200, header)
-      end
+      return applet_html
     end
+    
   end
 end
