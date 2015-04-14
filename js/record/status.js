6d51d6f1b9344dc8b0af6731994e34d4c108be2d
skymountain <ryukilon@gmail.com>
2015-04-14 13:33:11 +0900

    Modularize temrs for status

---
+++ js/record/status.js
@@ -0,0 +1,12 @@
+module.exports = {
+    terms: {
+        'none':     '未提出',
+        'OK':       '受理',
+        'NG':       '要再提出',
+        'build':    '確認中',
+        'check':    '確認中',
+        'build:NG': '要再提出',
+        'check:NG': '要再提出',
+        'report':   'レポート確認中',
+    }
+};
