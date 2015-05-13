5d3d7d96732b8ed81bbf07205b78032de4019bf5
OKU_K <oku.87.s62.a4@gmail.com>
2015-05-13 16:15:49 +0900

    Add comments on lib/string/random.rb.

--- lib/string/random.rb
+++ lib/string/random.rb
@@ -1,8 +1,15 @@
 class String
+  # Return a random string which includes characters of a reciever
+  # @param [Integer] len the length of the returned string
   def randomize(len=128)
     return Array.new(len).map{ self[rand(self.length)].chr }.join
   end
 
+  # Return a random string
+  # @param [Integer] len the length of the returned string
+  # @param [String] src characters which may be included by the returned string
+  # @return [String] a random string which includes characters from src,
+  #                  or lowercases, uppercases, numerals, and some symbols if src is nil
   def self.random(len=128, src=nil)
     src ||=
       [
